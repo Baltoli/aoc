876c7faa8612220319b89a1fc70f0d3e01ee88e4
Bruce Collie <brucecollie82@gmail.com>
2019-12-05 15:04:58 +0000

    Implement day 1/1

--- src/src/day1.cpp
+++ src/src/day1.cpp
@@ -1,1 +1,21 @@
-int main() {}
+#include <cassert>
+#include <iostream>
+#include <string>
+
+int get_fuel_req(int mass) { return (mass / 3) - 2; }
+
+int main() {
+  assert(get_fuel_req(12) == 2);
+  assert(get_fuel_req(14) == 2);
+  assert(get_fuel_req(1969) == 654);
+  assert(get_fuel_req(100756) == 33583);
+
+  int total_fuel = 0;
+
+  for (std::string line; std::getline(std::cin, line);) {
+    int mass = std::stoi(line);
+    total_fuel += get_fuel_req(mass);
+  }
+
+  std::cout << total_fuel << '\n';
+}

2ae4085fe5bf0f9e0adc18fd3c5661c699486c7e
Bruce Collie <brucecollie82@gmail.com>
2019-12-12 00:17:05 +0000

    Implement day11 / 1

--- src/day11.cpp
+++ src/day11.cpp
@@ -1,5 +1,7 @@
 #include <intcode.h>
 
+#include <cassert>
+#include <iostream>
 #include <map>
 #include <string>
 
@@ -9,32 +11,40 @@ class painter {
 public:
   painter(std::string const&);
 
-  void paint(loc_t loc, int col);
-  int  get(loc_t loc) const;
+  void run();
 
   int count() const;
 
 private:
+  void paint(int col);
+  int  get() const;
+  void left();
+  void right();
+  void step();
+
   int comp_out_;
 
+  int                  dir_;
   loc_t                loc_;
   ic::computer         computer_;
   std::map<loc_t, int> grid_;
 };
 
 painter::painter(std::string const& prog)
-    : comp_out_(0)
+    : dir_(0)
+    , comp_out_(0)
     , loc_ {0, 0}
-    , computer_(prog, [this](auto i) { comp_out_ = i; })
+    , computer_(
+          prog, [this](auto i) { comp_out_ = i; }, true)
     , grid_ {}
 {
 }
 
-void painter::paint(loc_t loc, int col) { grid_[loc] = col; }
+void painter::paint(int col) { grid_[loc_] = col; }
 
-int painter::get(loc_t loc) const
+int painter::get() const
 {
-  auto lookup = grid_.find(loc);
+  auto lookup = grid_.find(loc_);
   if (lookup != grid_.end()) {
     return lookup->second;
   } else {
@@ -42,6 +52,64 @@ int painter::get(loc_t loc) const
   }
 }
 
+void painter::left() { dir_ = (dir_ + 3) % 4; }
+
+void painter::right() { dir_ = (dir_ + 1) % 4; }
+
+void painter::step()
+{
+  switch (dir_) {
+  case 0:
+    loc_.second--;
+    break;
+  case 1:
+    loc_.first++;
+    break;
+  case 2:
+    loc_.second++;
+    break;
+  case 3:
+    loc_.first--;
+    break;
+  default:
+    assert(false && "Bad step");
+  }
+}
+
 int painter::count() const { return grid_.size(); }
 
-int main() {}
+void painter::run()
+{
+  while (!computer_.halted()) {
+    computer_.input(get());
+
+    computer_.run();
+    int col = comp_out_;
+
+    computer_.run();
+    int turn = comp_out_;
+
+    paint(col);
+
+    if (turn == 0) {
+      left();
+    } else if (turn == 1) {
+      right();
+    } else {
+      assert(false && "Bad turn");
+    }
+
+    step();
+  }
+}
+
+int main()
+{
+  std::string line;
+  std::getline(std::cin, line);
+
+  auto paint = painter(line);
+  paint.run();
+
+  std::cout << paint.count() << '\n';
+}

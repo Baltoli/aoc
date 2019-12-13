3f80f2a946cebdc746834c4e2bd0a52c5538b605
Bruce Collie <brucecollie82@gmail.com>
2019-12-13 10:36:07 +0000

    Implement day12/1

--- src/day12.cpp
+++ src/day12.cpp
@@ -11,6 +11,8 @@ struct planet {
 
   int energy() const;
 
+  void dump() const;
+
   vec3 position;
   vec3 velocity = {0, 0, 0};
 };
@@ -26,16 +28,47 @@ struct orbit {
 
 planet::planet(std::string const& pos)
 {
-  ;
-  ;
+  auto coords = std::array {'x', 'y', 'z'};
+  for (int i = 0; i < 3; ++i) {
+    auto start  = pos.find(coords[i]) + 2;
+    auto end    = pos.find(',', start);
+    position[i] = std::stoi(pos.substr(start, end - start));
+  }
 }
 
-int planet::energy() const { return 0; }
+int planet::energy() const
+{
+  auto abs_acc = [](auto a, auto c) { return a + std::abs(c); };
+
+  auto pot = std::accumulate(position.begin(), position.end(), 0, abs_acc);
+  auto kin = std::accumulate(velocity.begin(), velocity.end(), 0, abs_acc);
+
+  return pot * kin;
+}
 
 void orbit::step()
 {
-  ;
-  ;
+  for (auto& p1 : planets) {
+    for (auto& p2 : planets) {
+      if (&p1 < &p2) {
+        for (int i = 0; i < 3; ++i) {
+          if (p1.position[i] > p2.position[i]) {
+            p1.velocity[i]--;
+            p2.velocity[i]++;
+          } else if (p1.position[i] < p2.position[i]) {
+            p1.velocity[i]++;
+            p2.velocity[i]--;
+          }
+        }
+      }
+    }
+  }
+
+  for (auto& p : planets) {
+    for (int i = 0; i < 3; ++i) {
+      p.position[i] += p.velocity[i];
+    }
+  }
 }
 
 int orbit::energy() const
@@ -43,6 +76,13 @@ int orbit::energy() const
   return std::accumulate(
       planets.begin(), planets.end(), 0,
       [](auto acc, auto p) { return acc + p.energy(); });
+}
+
+void planet::dump() const
+{
+  std::cout << "pos<x=" << position[0] << ", y=" << position[1]
+            << ", z=" << position[2] << ">, vel=<x=" << velocity[0]
+            << ", y=" << velocity[1] << ", z=" << velocity[2] << ">\n";
 }
 
 int main()

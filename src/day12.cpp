91be900df2a8ffe2a1c12b84ecb8b6ad1a8a37f8
Bruce Collie <brucecollie82@gmail.com>
2019-12-14 09:28:10 +0000

    Implement day 12/2

--- src/day12.cpp
+++ src/day12.cpp
@@ -1,15 +1,16 @@
 #include <array>
 #include <iostream>
 #include <numeric>
+#include <set>
 #include <string>
 #include <vector>
 
-using vec3 = std::array<int, 3>;
+using vec3 = std::array<long, 3>;
 
 struct planet {
   planet(std::string const& pos);
 
-  int energy() const;
+  long energy() const;
 
   void dump() const;
 
@@ -21,22 +22,27 @@ struct orbit {
   orbit() = default;
 
   void step();
-  int  energy() const;
+  long energy() const;
+  long get_period() const;
 
   std::vector<planet> planets = {};
+
+  std::array<long, 3>                        period      = {0, 0, 0};
+  long                                       steps       = 0;
+  std::array<std::set<std::vector<long>>, 3> past_states = {};
 };
 
 planet::planet(std::string const& pos)
 {
   auto coords = std::array {'x', 'y', 'z'};
-  for (int i = 0; i < 3; ++i) {
+  for (long i = 0; i < 3; ++i) {
     auto start  = pos.find(coords[i]) + 2;
     auto end    = pos.find(',', start);
     position[i] = std::stoi(pos.substr(start, end - start));
   }
 }
 
-int planet::energy() const
+long planet::energy() const
 {
   auto abs_acc = [](auto a, auto c) { return a + std::abs(c); };
 
@@ -48,10 +54,25 @@ int planet::energy() const
 
 void orbit::step()
 {
+  auto states = std::array<std::vector<long>, 3> {};
+  for (long i = 0; i < 3; ++i) {
+    for (auto const& p : planets) {
+      states[i].push_back(p.position[i]);
+      states[i].push_back(p.velocity[i]);
+    }
+
+    if (period[i] == 0
+        && past_states[i].find(states[i]) != past_states[i].end()) {
+      period[i] = steps;
+    }
+
+    past_states[i].insert(states[i]);
+  }
+
   for (auto& p1 : planets) {
     for (auto& p2 : planets) {
       if (&p1 < &p2) {
-        for (int i = 0; i < 3; ++i) {
+        for (long i = 0; i < 3; ++i) {
           if (p1.position[i] > p2.position[i]) {
             p1.velocity[i]--;
             p2.velocity[i]++;
@@ -65,17 +86,31 @@ void orbit::step()
   }
 
   for (auto& p : planets) {
-    for (int i = 0; i < 3; ++i) {
+    for (long i = 0; i < 3; ++i) {
       p.position[i] += p.velocity[i];
     }
   }
+
+  steps++;
 }
 
-int orbit::energy() const
+long orbit::energy() const
 {
   return std::accumulate(
       planets.begin(), planets.end(), 0,
       [](auto acc, auto p) { return acc + p.energy(); });
+}
+
+long orbit::get_period() const
+{
+  if (std::any_of(
+          period.begin(), period.end(), [](auto p) { return p == 0; })) {
+    return 0;
+  }
+
+  return std::accumulate(
+      period.begin(), period.end(), 1L,
+      [](auto acc, auto p) { return std::lcm(acc, p); });
 }
 
 void planet::dump() const
@@ -91,10 +126,17 @@ int main()
   for (std::string line; std::getline(std::cin, line);) {
     sys.planets.emplace_back(line);
   }
+  auto s2 = sys;
 
-  for (int i = 0; i < 1000; ++i) {
+  for (long i = 0; i < 1000; ++i) {
     sys.step();
   }
 
   std::cout << sys.energy() << '\n';
+
+  while (s2.get_period() == 0) {
+    s2.step();
+  }
+
+  std::cout << s2.get_period() << '\n';
 }

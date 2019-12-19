166c5718cbc82d49c29546905ed1ac72512ce126
Bruce Collie <brucecollie82@gmail.com>
2019-12-19 14:12:44 +0000

    Inefficient day 16/1

--- src/day16.cpp
+++ src/day16.cpp
@@ -4,6 +4,8 @@
 #include <string>
 #include <vector>
 
+using lookup_table = std::vector<std::vector<int>>;
+
 std::vector<int> read_input()
 {
   std::string line;
@@ -18,11 +20,13 @@ std::vector<int> read_input()
   return ret;
 }
 
-std::vector<std::vector<int>> patterns(int max_len)
+int ones(int in) { return std::abs(in % 10); }
+
+lookup_table patterns(int max_len)
 {
   auto base_pattern = std::array {0, 1, 0, -1};
 
-  auto ret = std::vector<std::vector<int>> {};
+  auto ret = lookup_table {};
   ret.reserve(max_len);
 
   for (int digit = 0; digit < max_len; ++digit) {
@@ -42,8 +46,37 @@ std::vector<std::vector<int>> patterns(int max_len)
   return ret;
 }
 
+std::vector<int>
+fft_phase(std::vector<int> const& data, lookup_table const& lut)
+{
+  assert(data.size() == lut.size() && "Lookup is the wrong size");
+  auto ret = std::vector<int>(data);
+
+  for (int i = 0; i < data.size(); ++i) {
+    auto const& pat = lut[i];
+    auto        sum = 0;
+
+    for (int j = 0; j < data.size(); ++j) {
+      sum += ret[j] * pat[j];
+    }
+
+    ret[i] = ones(sum);
+  }
+
+  return ret;
+}
+
 int main()
 {
   auto in   = read_input();
   auto pats = patterns(in.size());
+
+  for (int i = 0; i < 100; ++i) {
+    in = fft_phase(in, pats);
+  }
+
+  for (int i = 0; i < 8; ++i) {
+    std::cout << in[i];
+  }
+  std::cout << '\n';
 }

bd1bee997c7efea36d3f31e544367054beceb3d7
Bruce Collie <brucecollie82@gmail.com>
2019-12-19 10:13:13 +0000

    Compute luts

--- src/day16.cpp
+++ src/day16.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <array>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -17,8 +18,32 @@ std::vector<int> read_input()
   return ret;
 }
 
+std::vector<std::vector<int>> patterns(int max_len)
+{
+  auto base_pattern = std::array {0, 1, 0, -1};
+
+  auto ret = std::vector<std::vector<int>> {};
+  ret.reserve(max_len);
+
+  for (int digit = 0; digit < max_len; ++digit) {
+    auto pattern = std::vector<int> {};
+    pattern.reserve(max_len);
+
+    for (int value = 0; value < max_len + 1; ++value) {
+      auto base = (value / (digit + 1)) % base_pattern.size();
+      if (value > 0) {
+        pattern.push_back(base_pattern[base]);
+      }
+    }
+
+    ret.push_back(pattern);
+  }
+
+  return ret;
+}
+
 int main()
 {
-  auto in = read_input();
-  ;
+  auto in   = read_input();
+  auto pats = patterns(in.size());
 }

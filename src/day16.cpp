d991dc70ed733475c39bb309d38bfa77d4c7d443
Bruce Collie <brucecollie82@gmail.com>
2019-12-19 09:37:20 +0000

    Read input for day 16

--- src/day16.cpp
+++ src/day16.cpp
@@ -1,1 +1,24 @@
-int main() {}
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::vector<int> read_input()
+{
+  std::string line;
+  std::getline(std::cin, line);
+
+  auto ret = std::vector<int> {};
+
+  std::transform(line.begin(), line.end(), std::back_inserter(ret), [](auto c) {
+    return c - '0';
+  });
+
+  return ret;
+}
+
+int main()
+{
+  auto in = read_input();
+  ;
+}

92e0519f0864317e6d9e9d4e0396151007fc94a9
Bruce Collie <brucecollie82@gmail.com>
2019-12-20 19:45:26 +0000

    Load map

--- src/day18.cpp
+++ src/day18.cpp
@@ -1,1 +1,34 @@
-int main() {}
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+class maze {
+public:
+  maze();
+
+private:
+  std::map<char, std::pair<int, int>> keys_;
+  std::map<char, std::pair<int, int>> doors_;
+  std::vector<std::vector<char>>      map_;
+};
+
+maze::maze()
+    : map_ {}
+{
+  for (std::string line; std::getline(std::cin, line);) {
+    map_.emplace_back(line.begin(), line.end());
+  }
+
+  for (int y = 0; y < map_.size(); ++y) {
+    for (int x = 0; x < map_[y].size(); ++x) {
+      if (std::islower(map_[y][x])) {
+        keys_[map_[y][x]] = {x, y};
+      } else if (std::isupper(map_[y][x])) {
+        doors_[std::tolower(map_[y][x])] = {x, y};
+      }
+    }
+  }
+}
+
+int main() { auto m = maze(); }

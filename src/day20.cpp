ab4cf0a52e462eada43e3ac72d59b26db2f885a8
Bruce Collie <brucecollie82@gmail.com>
2019-12-22 22:30:32 +0000

    Parsing for day 20

---
+++ src/day20.cpp
@@ -0,0 +1,106 @@
+#include <array>
+#include <cassert>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+using loc = std::pair<int, int>;
+
+std::string from_chars(char a, char b)
+{
+  auto arr = std::array {a, b};
+  return std::string(arr.begin(), arr.end());
+}
+
+class maze {
+public:
+  maze();
+
+  int width() const;
+  int height() const;
+
+  char at(int x, int y) const;
+
+  void dump() const;
+
+private:
+  std::vector<std::vector<char>> map_;
+
+  std::map<loc, loc> portals_;
+  loc                start_;
+  loc                end_;
+};
+
+maze::maze()
+    : map_ {}
+    , start_ {0, 0}
+    , end_ {0, 0}
+{
+  for (std::string line; std::getline(std::cin, line);) {
+    map_.emplace_back(line.begin(), line.end());
+  }
+
+  auto key_locs = std::map<std::string, std::vector<loc>> {};
+
+  for (int y = 0; y < height(); ++y) {
+    for (int x = 0; x < width() - 1; ++x) {
+      if (std::isupper(at(x, y)) && std::isupper(at(x + 1, y))) {
+        auto str = from_chars(at(x, y), at(x + 1, y));
+        key_locs.try_emplace(str);
+
+        if (at(x - 1, y) == '.') {
+          key_locs[str].push_back({x - 1, y});
+        } else if (x + 2 < width() && at(x + 2, y) == '.') {
+          key_locs[str].push_back({x + 2, y});
+        }
+      }
+    }
+  }
+
+  for (int x = 0; x < width(); ++x) {
+    for (int y = 0; y < height() - 1; ++y) {
+      if (std::isupper(at(x, y)) && std::isupper(at(x, y + 1))) {
+        auto str = from_chars(at(x, y), at(x, y + 1));
+        key_locs.try_emplace(str);
+
+        if (y > 0 && at(x, y - 1) == '.') {
+          key_locs[str].push_back({x, y - 1});
+        } else if (y + 2 < width() && at(x, y + 2) == '.') {
+          key_locs[str].push_back({x, y + 2});
+        }
+      }
+    }
+  }
+
+  start_ = key_locs["AA"][0];
+  end_   = key_locs["ZZ"][0];
+
+  for (auto const& [key, locs] : key_locs) {
+    if (locs.size() == 2) {
+      portals_[locs[0]] = locs[1];
+      portals_[locs[1]] = locs[0];
+    }
+  }
+}
+
+int maze::width() const { return map_[0].size(); }
+int maze::height() const { return map_.size(); }
+
+char maze::at(int x, int y) const { return map_[y][x]; }
+
+void maze::dump() const
+{
+  for (auto const& row : map_) {
+    for (auto c : row) {
+      std::cout << c;
+    }
+    std::cout << '\n';
+  }
+}
+
+int main()
+{
+  auto m = maze();
+  m.dump();
+}

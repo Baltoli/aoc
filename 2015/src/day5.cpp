5df1e9ed0858bc4a5ad75ca3c3bdebaf932e3507
Bruce Collie 80054962 <bruce.collie@huawei.com>
2020-01-06 16:39:57 +0000

    Implement 2015 day 5/2

--- 2015/src/day5.cpp
+++ 2015/src/day5.cpp
@@ -36,11 +36,44 @@ bool nice(std::string const& s)
   return vowels(s) && consecutive(s) && bad_strings(s);
 }
 
+bool pairs(std::string const& s)
+{
+  for (int i = 0; i < s.size() - 1; ++i) {
+    auto sub = s.substr(i, 2);
+    for (int j = 0; j < s.size() - 1; ++j) {
+      if (std::abs(i - j) > 1) {
+        auto sub2 = s.substr(j, 2);
+        if (sub == sub2) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
+
+bool threes(std::string const& s)
+{
+  for (int i = 0; i < s.size() - 2; ++i) {
+    if (s[i] == s[i + 2]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool nice_v2(std::string const& s) { return pairs(s) && threes(s); }
+
 int main()
 {
   int nices = 0;
+  int v2s   = 0;
 
-  utils::for_each_line([&](auto line) { nices += nice(line); });
+  utils::for_each_line([&](auto line) {
+    nices += nice(line);
+    v2s += nice_v2(line);
+  });
 
   std::cout << nices << '\n';
+  std::cout << v2s << '\n';
 }

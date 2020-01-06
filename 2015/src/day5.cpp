8bd099f6be5ab72b3a630035e898ddbbb6a04f82
Bruce Collie 80054962 <bruce.collie@huawei.com>
2020-01-06 16:30:35 +0000

    Implement 2015 day 5/1

---
+++ 2015/src/day5.cpp
@@ -0,0 +1,46 @@
+#include <utils/utils.h>
+
+#include <algorithm>
+#include <array>
+#include <string>
+
+bool vowels(std::string const& s)
+{
+  auto count = 0;
+  for (auto c : {'a', 'e', 'i', 'o', 'u'}) {
+    count += std::count(s.begin(), s.end(), c);
+  }
+  return count >= 3;
+}
+
+bool consecutive(std::string const& s)
+{
+  for (int i = 0; i < s.size() - 1; ++i) {
+    if (s[i] == s[i + 1]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool bad_strings(std::string const& s)
+{
+  auto bads = std::array {"ab", "cd", "pq", "xy"};
+  return std::none_of(bads.begin(), bads.end(), [&](auto bad) {
+    return s.find(bad) != std::string::npos;
+  });
+}
+
+bool nice(std::string const& s)
+{
+  return vowels(s) && consecutive(s) && bad_strings(s);
+}
+
+int main()
+{
+  int nices = 0;
+
+  utils::for_each_line([&](auto line) { nices += nice(line); });
+
+  std::cout << nices << '\n';
+}

b5740f66f0d24cc0baf33bcf67cc8c8bce905ab9
Bruce Collie 80054962 <bruce.collie@huawei.com>
2020-01-06 15:58:10 +0000

    Implement 2015 day 4

--- 2015/src/day4.cpp
+++ 2015/src/day4.cpp
@@ -1,7 +1,28 @@
 #include <utils/utils.h>
 
+#include <algorithm>
+
+std::string input(int i) { return "iwrupvqb" + std::to_string(i); }
+
+bool valid(int i, int len)
+{
+  auto hash = utils::md5_sum(input(i));
+  return std::all_of(
+      hash.begin(), hash.begin() + len, [](auto c) { return c == '0'; });
+}
+
+void task(int len)
+{
+  int i = 1;
+  while (!valid(i, len)) {
+    ++i;
+  }
+
+  std::cout << i << '\n';
+}
+
 int main()
 {
-  std::string s = "woo";
-  std::cout << utils::md5_sum(s) << '\n';
+  task(5);
+  task(6);
 }

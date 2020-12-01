d1057ce4d1c3e0919bb8e3c0412bb02acfd5f989
Bruce Collie 80054962 <bruce.collie@huawei.com>
2020-12-01 13:56:00 +0000

    2020 day 1 part 2

--- 2020/src/day1.cpp
+++ 2020/src/day1.cpp
@@ -2,20 +2,62 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <map>
+#include <optional>
 #include <set>
+#include <utility>
+#include <vector>
 
-int main()
+std::vector<int> get_input()
+{
+  auto ret = std::vector<int> {};
+  utils::for_each_line(
+      [&ret](auto const& line) { ret.push_back(std::atoi(line.c_str())); });
+  return ret;
+}
+
+std::optional<std::pair<int, int>>
+pair_sum_to(std::vector<int> const& cont, int sum, int exclude = 0)
 {
   auto reqs = std::set<int> {};
 
-  utils::for_each_line([&](auto const& line) {
-    auto val = std::atoi(line.c_str());
-
-    if (reqs.find(val) != reqs.end()) {
-      std::cout << (val * (2020 - val)) << '\n';
-      return;
+  for (auto val : cont) {
+    if (val == exclude) {
+      return std::nullopt;
     }
 
-    reqs.insert(2020 - val);
-  });
+    if (reqs.find(val) != reqs.end()) {
+      return std::pair {val, sum - val};
+    }
+
+    reqs.insert(sum - val);
+  }
+
+  return std::nullopt;
+}
+
+void part_1(std::vector<int> const& in)
+{
+  if (auto p = pair_sum_to(in, 2020)) {
+    std::cout << (p->first * p->second) << '\n';
+  }
+}
+
+void part_2(std::vector<int> const& in)
+{
+  for (auto val : in) {
+    auto remain = 2020 - val;
+
+    if (auto p = pair_sum_to(in, remain, val)) {
+      std::cout << (val * p->first * p->second) << '\n';
+    }
+  }
+}
+
+int main()
+{
+  auto in = get_input();
+
+  part_1(in);
+  part_2(in);
 }

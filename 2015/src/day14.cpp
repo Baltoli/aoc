91c47b262b7c3e21a6d1fa5ee51cf81180eaac6e
Bruce Collie <brucecollie82@gmail.com>
2020-12-06 12:07:44 +0000

    2015 day 14 part 2

--- 2015/src/day14.cpp
+++ 2015/src/day14.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <iostream>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 using namespace ctre::literals;
@@ -12,6 +13,8 @@ constexpr ctll::fixed_string name_  = "name";
 constexpr ctll::fixed_string speed_ = "speed";
 constexpr ctll::fixed_string time_  = "time";
 constexpr ctll::fixed_string rest_  = "rest";
+
+constexpr auto problem_time = 2503;
 
 struct reindeer {
   template <typename Match>
@@ -40,10 +43,41 @@ struct reindeer {
   int         rest_time;
 };
 
+int part_1(std::vector<reindeer> const& rs, int time)
+{
+  auto best_r = std::max_element(
+      rs.begin(), rs.end(), [&](auto const& a, auto const& b) {
+        return a.distance(time) < b.distance(time);
+      });
+
+  return best_r->distance(time);
+}
+
+int part_2(std::vector<reindeer> const& rs)
+{
+  auto scores = std::unordered_map<std::string, int> {};
+
+  for (auto i = 1; i <= problem_time; ++i) {
+    auto max_dist = part_1(rs, i);
+
+    for (auto const& r : rs) {
+      scores.try_emplace(r.name, 0);
+      if (r.distance(i) == max_dist) {
+        scores[r.name] += 1;
+      }
+    }
+  }
+
+  return std::max_element(
+             scores.begin(), scores.end(),
+             [](auto const& p1, auto const& p2) {
+               return p1.second < p2.second;
+             })
+      ->second;
+}
+
 int main()
 {
-  constexpr auto problem_time = 2503;
-
   constexpr auto line_pattern
       = "(?<name>[A-Z][a-z]+) can fly (?<speed>\\d+) km/s for (?<time>\\d+) seconds, but then must rest for (?<rest>\\d+) seconds."_ctre;
 
@@ -51,10 +85,6 @@ int main()
   utils::for_each_line(
       [&](auto const& line) { rs.emplace_back(line_pattern.match(line)); });
 
-  auto best_r = std::max_element(
-      rs.begin(), rs.end(), [&](auto const& a, auto const& b) {
-        return a.distance(problem_time) < b.distance(problem_time);
-      });
-
-  std::cout << best_r->distance(problem_time) << '\n';
+  std::cout << part_1(rs, problem_time) << '\n';
+  std::cout << part_2(rs) << '\n';
 }

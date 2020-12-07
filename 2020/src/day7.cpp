e90cc095145462f981946c03812646c2dbf496bc
Bruce Collie <brucecollie82@gmail.com>
2020-12-07 17:15:09 +0000

    TODO

--- 2020/src/day7.cpp
+++ 2020/src/day7.cpp
@@ -5,6 +5,7 @@
 #include <map>
 #include <queue>
 #include <set>
+#include <stack>
 #include <string>
 #include <string_view>
 #include <utility>
@@ -18,7 +19,7 @@ constexpr ctll::fixed_string rest_   = "rest";
 constexpr ctll::fixed_string none_   = "none";
 constexpr ctll::fixed_string num_    = "num";
 
-adj_t read_adjacencies()
+std::pair<adj_t, adj_t> read_adjacencies()
 {
   constexpr auto line_pattern
       = "(?<colour>[a-z]+ [a-z]+) bags contain (?<rest>.*)\\."_ctre;
@@ -26,7 +27,8 @@ adj_t read_adjacencies()
   constexpr auto rest_pattern
       = "((?<none>no other bags))|((?<num>\\d+) (?<colour>[a-z]+ [a-z]+) bags?(, (?<rest>.*))?)"_ctre;
 
-  auto ret = adj_t {};
+  auto upwards   = adj_t {};
+  auto downwards = adj_t {};
 
   utils::for_each_line([&](auto const& line) {
     auto m         = line_pattern.match(line);
@@ -44,13 +46,17 @@ adj_t read_adjacencies()
       auto to_col = rm.template get<colour_>().str();
       auto num    = rm.template get<num_>().str();
 
-      ret.try_emplace(col);
-      ret.try_emplace(to_col);
-      ret.at(to_col).emplace(col, utils::svtoi(num));
+      upwards.try_emplace(col);
+      downwards.try_emplace(col);
+      upwards.try_emplace(to_col);
+      downwards.try_emplace(to_col);
+
+      upwards.at(to_col).emplace(col, utils::svtoi(num));
+      downwards.at(col).emplace(to_col, utils::svtoi(num));
     }
   });
 
-  return ret;
+  return {upwards, downwards};
 }
 
 int unique_can_reach(std::string const& key, adj_t const& adj)
@@ -80,8 +86,15 @@ int unique_can_reach(std::string const& key, adj_t const& adj)
   return count - 1;
 }
 
+int contained(std::string const& key, adj_t const& adj)
+{
+  // TODO?
+  return 0;
+}
+
 int main()
 {
-  auto adj = read_adjacencies();
-  std::cout << unique_can_reach("shiny gold", adj) << '\n';
+  auto [up, down] = read_adjacencies();
+  std::cout << unique_can_reach("shiny gold", up) << '\n';
+  std::cout << contained("shiny gold", down) << '\n';
 }

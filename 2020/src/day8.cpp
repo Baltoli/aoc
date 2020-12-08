e7cceb91c43bfb20573d11ad68c018df48e8b057
Bruce Collie <brucecollie82@gmail.com>
2020-12-08 16:07:32 +0000

    2020 day 8 part 1

--- 2020/src/day8.cpp
+++ 2020/src/day8.cpp
@@ -18,10 +18,62 @@ struct instruction {
   int         arg;
 };
 
+struct computer {
+  computer(std::vector<instruction> const& p)
+      : program(p)
+      , executed(p.size(), false)
+      , accumulator(0)
+      , pc(0)
+  {
+  }
+
+  void step()
+  {
+    auto const& inst = program.at(pc);
+
+    if (inst.opcode == "nop") {
+      pc += 1;
+      return;
+    }
+
+    if (inst.opcode == "acc") {
+      accumulator += inst.arg;
+      pc += 1;
+      return;
+    }
+
+    if (inst.opcode == "jmp") {
+      pc += inst.arg;
+      return;
+    }
+  }
+
+  void run_until_loop()
+  {
+    while (true) {
+      if (executed.at(pc)) {
+        return;
+      }
+
+      executed.at(pc) = true;
+      step();
+    }
+  }
+
+  std::vector<instruction> const& program;
+  std::vector<bool>               executed;
+  int                             accumulator;
+  int                             pc;
+};
+
 int main()
 {
   auto instrs = utils::map_lines([](auto const& line) {
     constexpr auto pattern = "([a-z]{3}) ((\\+|-)\\d+)"_ctre;
     return instruction(pattern.match(line));
   });
+
+  auto comp = computer(instrs);
+  comp.run_until_loop();
+  std::cout << comp.accumulator << '\n';
 }

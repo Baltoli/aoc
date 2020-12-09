4e57fd192096b361f4b235750f985d73de1878c5
Bruce Collie <brucecollie82@gmail.com>
2020-12-09 19:38:55 +0000

    2020 day 9 part 2

--- 2020/src/day9.cpp
+++ 2020/src/day9.cpp
@@ -52,8 +52,33 @@ long first_non_sum(int window, std::vector<long> const& nums)
   return 0;
 }
 
+long contiguous_sum(long target, std::vector<long> const& nums)
+{
+  auto low  = 0;
+  auto high = 1;
+  auto sum  = nums[low] + nums[high];
+
+  while (sum < target) {
+    auto test = sum + nums[high + 1];
+
+    if (test <= target) {
+      sum += nums[++high];
+    } else {
+      sum -= nums[low++];
+    }
+  }
+
+  auto b = nums.begin() + low;
+  auto e = nums.begin() + high;
+  return *std::min_element(b, e) + *std::max_element(b, e);
+}
+
 int main()
 {
-  auto in = get_input();
-  std::cout << first_non_sum(25, in) << '\n';
+  auto in    = get_input();
+  auto sum   = first_non_sum(25, in);
+  auto r_sum = contiguous_sum(sum, in);
+
+  std::cout << sum << '\n';
+  std::cout << r_sum << '\n';
 }

8a1debd81430c784905040a145dd860023726199
Bruce Collie <brucecollie82@gmail.com>
2020-12-10 22:07:11 +0000

    2020 day 10 part 2

--- 2020/src/day10.cpp
+++ 2020/src/day10.cpp
@@ -2,11 +2,12 @@
 
 #include <algorithm>
 #include <iostream>
+#include <unordered_map>
 #include <vector>
 
-std::vector<int> get_input() { return utils::map_lines(utils::stoi); }
+std::vector<long> get_input() { return utils::map_lines(utils::stol); }
 
-int part_1(std::vector<int> in)
+long part_1(std::vector<long> in)
 {
   std::sort(in.begin(), in.end());
 
@@ -23,19 +24,43 @@ int part_1(std::vector<int> in)
   return ones * threes;
 }
 
-int part_2(std::vector<int> in)
+long part_2_helper(
+    long idx, std::vector<long> const& in,
+    std::unordered_map<long, long>& paths)
 {
-  /*
-   * Flip the arrows and walk the graph backwards - paths(N) = sum_(s in suc(N))
-   * paths(s)
-   *
-   * Memoize a table to avoid recursive blowup
-   */
-  return 0;
+  if (paths.find(in[idx]) == paths.end()) {
+    auto sum = 0l;
+
+    for (auto off = 1; off < 4 && idx + off < in.size(); ++off) {
+      auto next = in[idx + off];
+      if (in[idx] - next <= 3) {
+        sum += part_2_helper(idx + off, in, paths);
+      }
+    }
+
+    paths[in[idx]] = sum;
+  }
+
+  return paths.at(in[idx]);
+}
+
+long part_2(std::vector<long> in)
+{
+  auto paths = std::unordered_map<long, long> {};
+
+  std::sort(in.begin(), in.end());
+  in.push_back(in[in.size() - 1] + 3);
+
+  std::reverse(in.begin(), in.end());
+  in.push_back(1);
+
+  paths[1] = 1;
+  return part_2_helper(0, in, paths);
 }
 
 int main()
 {
   auto in = get_input();
   std::cout << part_1(in) << '\n';
+  std::cout << part_2(in) << '\n';
 }

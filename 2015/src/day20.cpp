f9a1140941808982479426f52b126dc7bd0bfa5d
Bruce Collie <brucecollie82@gmail.com>
2020-12-22 20:13:30 +0000

    2020 day 20

---
+++ 2015/src/day20.cpp
@@ -0,0 +1,59 @@
+#include <utils/utils.h>
+
+#include <cmath>
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+long get_input()
+{
+  auto in = std::string {};
+  std::getline(std::cin, in);
+  return std::stol(in);
+}
+
+long first_to_n(long target)
+{
+  auto presents = std::vector<long>(target, 0);
+  for (auto elf = 1; elf < presents.size(); ++elf) {
+    for (auto house = elf; house < presents.size(); house += elf) {
+      presents.at(house) += elf * 10;
+    }
+  }
+
+  for (auto house = 1; house < presents.size(); ++house) {
+    if (presents.at(house) >= target) {
+      return house;
+    }
+  }
+
+  assert(false && "Solution not found");
+}
+
+long non_infinite(long target)
+{
+  auto presents = std::vector<long>(target, 0);
+  for (auto elf = 1; elf < presents.size(); ++elf) {
+    for (auto visit = 0; visit < 50; ++visit) {
+      auto house = elf + (visit * elf);
+      if (house < presents.size()) {
+        presents.at(house) += elf * 11;
+      }
+    }
+  }
+
+  for (auto house = 1; house < presents.size(); ++house) {
+    if (presents.at(house) >= target) {
+      return house;
+    }
+  }
+
+  assert(false && "Solution not found");
+}
+
+int main()
+{
+  auto in = get_input();
+  std::cout << first_to_n(in) << '\n';
+  std::cout << non_infinite(in) << '\n';
+}

cb4dc4ff02a5c5d3c3eca9a4ff6d825c7ab92cbc
Bruce Collie <brucecollie82@gmail.com>
2021-12-20 10:35:30 +0000

    2021 day 19

--- 2021/src/day19.cpp
+++ 2021/src/day19.cpp
@@ -13,52 +13,18 @@ struct coord {
 
   coord rotated(int r) const
   {
-    auto new_coord = *this;
+    auto n_x      = r & 1 ? -1 : 1;
+    auto n_y      = r & 2 ? -1 : 1;
+    auto n_z      = r & 4 ? -1 : 1;
+    auto distance = (r >> 3) & 0b111;
 
-    auto axis     = (r >> 3) & 0b11;
-    auto negate   = r & 0b1;
-    auto distance = (r >> 1) & 0b11;
-
-    auto neg_sign = negate ? -1 : 1;
-
-    auto rotate_parts = std::array<int, 4> {};
-    switch (axis) {
-    case 0:
-      rotate_parts = {+y, +z, -y, -z};
-      break;
-    case 1:
-      rotate_parts = {+x, +z, -x, -z};
-      break;
-    case 2:
-      rotate_parts = {+y, +x, -y, -x};
-      break;
+    auto indices = std::array {0, 1, 2};
+    for (int i = 0; i < distance; ++i) {
+      std::next_permutation(indices.begin(), indices.end());
     }
 
-    std::rotate(
-        rotate_parts.begin(), rotate_parts.begin() + distance,
-        rotate_parts.end());
-
-    switch (axis) {
-    case 0:
-      new_coord.x = x * neg_sign;
-      new_coord.y = rotate_parts[0];
-      new_coord.z = rotate_parts[1];
-      break;
-    case 1:
-      new_coord.x = rotate_parts[0];
-      new_coord.y = y * neg_sign;
-      new_coord.z = rotate_parts[1];
-      break;
-    case 2:
-      new_coord.x = rotate_parts[1];
-      new_coord.y = rotate_parts[0];
-      new_coord.z = z * neg_sign;
-      break;
-    }
-
-    auto rot_2 = std::array {new_coord.x, new_coord.y, new_coord.z};
-    std::rotate(rot_2.begin(), rot_2.begin() + axis, rot_2.end());
-    return {rot_2[0], rot_2[1], rot_2[2]};
+    auto vals = std::array {x * n_x, y * n_y, z * n_z};
+    return coord {vals[indices[0]], vals[indices[1]], vals[indices[2]]};
   }
 };
 
@@ -101,6 +67,15 @@ struct scanner {
   }
 };
 
+std::ostream& operator<<(std::ostream& os, scanner const& s)
+{
+  os << "--- scanner " << s.id << " ---\n";
+  for (auto const& c : s.beacons) {
+    os << c << '\n';
+  }
+  return os;
+}
+
 std::vector<scanner> load()
 {
   using namespace ctre::literals;
@@ -131,18 +106,67 @@ std::vector<scanner> load()
   return ret;
 }
 
+std::pair<bool, coord> align(scanner a, scanner b)
+{
+  for (auto const& a_beac : a.beacons) {
+    for (auto const& b_beac : b.beacons) {
+      auto dx = a_beac.x - b_beac.x;
+      auto dy = a_beac.y - b_beac.y;
+      auto dz = a_beac.z - b_beac.z;
+
+      auto overlap_count = std::count_if(
+          b.beacons.begin(), b.beacons.end(), [&](auto const& pre_translate) {
+            return a.beacons.contains(coord {
+                pre_translate.x + dx, pre_translate.y + dy,
+                pre_translate.z + dz});
+          });
+
+      if (overlap_count >= 12) {
+        return {true, {dx, dy, dz}};
+      }
+    }
+  }
+
+  return {false, {0, 0, 0}};
+}
+
 int main()
 {
   auto in = load();
 
-  // Part 1 logic
-  // for every pair of scanners:
-  //   not quite every pair - until all have been overlapped?
-  //   check all rotations of second one
-  //   shift so rotated and base have same smaller x coord
-  //    get the y + z translations from this
-  //   construct intersection of the two sets of points - if >= 12 then overlap
-  //   translate factor is x, y, z from before - add to all and construct global
-  //    set of points
-  //   beacon position relative to **other frame**
+  auto done_ids = std::unordered_set<int> {};
+  auto scanners = std::unordered_set<coord> {};
+
+  while (done_ids.size() < in.size() - 1) {
+    for (auto scan = 1; scan < in.size(); ++scan) {
+      if (!done_ids.contains(in[scan].id)) {
+        for (auto r = 0; r < 48; ++r) {
+          auto rotated_scan         = in[scan].rotated(r);
+          auto [aligned, translate] = align(in[0], rotated_scan);
+          if (aligned) {
+            for (auto const& a : rotated_scan.beacons) {
+              in[0].beacons.insert(coord {
+                  a.x + translate.x, a.y + translate.y, a.z + translate.z});
+            }
+
+            done_ids.insert(rotated_scan.id);
+            scanners.insert(translate);
+          }
+        }
+      }
+    }
+  }
+  std::cout << in[0].beacons.size() << '\n';
+
+  auto max_dist = std::numeric_limits<int>::min();
+  for (auto a : scanners) {
+    for (auto b : scanners) {
+      if (a != b) {
+        auto dist
+            = std::abs(a.x - b.x) + std::abs(a.y - b.y) + std::abs(a.z - b.z);
+        max_dist = std::max(dist, max_dist);
+      }
+    }
+  }
+  std::cout << max_dist << '\n';
 }

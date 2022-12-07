be1b5db5ab8b0ebef24229e91625d6a62005cc7c
Bruce Collie <brucecollie82@gmail.com>
2022-12-07 23:13:55 +0000

    2022 day 4

--- 2022/src/day4.cpp
+++ 2022/src/day4.cpp
@@ -1,3 +1,55 @@
 #include <utils/utils.h>
 
-int main() { }
+#include <cassert>
+#include <iostream>
+
+class range {
+public:
+  range(std::string const& line)
+  {
+    auto parts = utils::split(line, "-");
+    assert(parts.size() == 2 && "Wrong split size");
+
+    begin_ = utils::svtol(parts[0]);
+    end_   = utils::svtol(parts[1]);
+  }
+
+  bool contains(range const& other)
+  {
+    return begin_ <= other.begin_ && end_ >= other.end_;
+  }
+
+  bool overlaps(range const& other)
+  {
+    return !(end_ < other.begin_ || begin_ > other.end_);
+  }
+
+private:
+  long begin_;
+  long end_;
+};
+
+int main()
+{
+  auto total_1 = 0;
+  auto total_2 = 0;
+
+  utils::for_each_line([&](auto const& line) {
+    auto parts = utils::split(line, ",");
+    assert(parts.size() == 2 && "Wrong split size");
+
+    auto a = range(parts[0]);
+    auto b = range(parts[1]);
+
+    if (a.contains(b) || b.contains(a)) {
+      total_1 += 1;
+    }
+
+    if (a.overlaps(b)) {
+      total_2 += 1;
+    }
+  });
+
+  std::cout << total_1 << '\n';
+  std::cout << total_2 << '\n';
+}

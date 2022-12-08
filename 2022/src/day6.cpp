b9eeda9b3b8e83cfecfc656ff082d40d0bb52313
Bruce Collie <brucecollie82@gmail.com>
2022-12-08 22:41:26 +0000

    2022 day 6

--- 2022/src/day6.cpp
+++ 2022/src/day6.cpp
@@ -1,3 +1,64 @@
 #include <utils/utils.h>
 
-int main() { }
+#include <iostream>
+#include <set>
+#include <vector>
+
+template <typename T>
+class ring_buffer {
+public:
+  ring_buffer(size_t c)
+      : capacity_(c)
+      , next_(0)
+  {
+  }
+
+  bool push(T item)
+  {
+    if (data_.size() < capacity_) {
+      data_.push_back(item);
+      return false;
+    } else {
+      data_[next_] = item;
+      next_        = (next_ + 1) % capacity_;
+      return true;
+    }
+  }
+
+  bool marker() const
+  {
+    auto seen = std::set<T> {};
+    for (auto d : data_) {
+      seen.insert(d);
+    }
+    return seen.size() == capacity_;
+  }
+
+private:
+  size_t         capacity_;
+  size_t         next_;
+  std::vector<T> data_;
+};
+
+int identify(std::string const& data, size_t len)
+{
+  auto buf = ring_buffer<char>(len);
+
+  for (auto i = 0u; i < data.size(); ++i) {
+    buf.push(data[i]);
+
+    if (buf.marker()) {
+      return i + 1;
+    }
+  }
+
+  return -1;
+}
+
+int main()
+{
+  auto input = utils::get_single_line();
+
+  std::cout << identify(input, 4) << '\n';
+  std::cout << identify(input, 14) << '\n';
+}

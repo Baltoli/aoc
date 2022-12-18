f3c35b52f40c9d3fcd6e7fbdb54e8fa5982d4d42
Bruce Collie <brucecollie82@gmail.com>
2022-12-18 08:34:42 +0000

    Fix

--- 2022/src/day11.cpp
+++ 2022/src/day11.cpp
@@ -41,20 +41,29 @@ class monkey;
 class game {
 public:
   game()
-      : monkeys_ {}
+      : field_(1)
+      , monkeys_ {}
   {
   }
 
+  void add(monkey m);
+
   void throw_to(int monkey, int worry);
 
+  void play_rounds(long);
+
+  long score() const;
+
 private:
+  long                field_;
   std::vector<monkey> monkeys_;
 };
 
 class monkey {
 public:
   monkey(std::vector<std::string>& input)
-      : items_ {}
+      : count_(0)
+      , items_ {}
       , lhs_(std::nullopt)
       , rhs_(std::nullopt)
       , op_ {}
@@ -83,11 +92,11 @@ public:
     modulus_     = utils::svtoi(mod_str.get<1>());
 
     auto true_str = ctre::match<"    If true: throw to monkey (.*)">(input[4]);
-    if_t_         = utils::svtoi(mod_str.get<1>());
+    if_t_         = utils::svtoi(true_str.get<1>());
 
     auto false_str
         = ctre::match<"    If false: throw to monkey (.*)">(input[5]);
-    if_f_ = utils::svtoi(mod_str.get<1>());
+    if_f_ = utils::svtoi(false_str.get<1>());
 
     assert(input.size() >= 7 && "Bad input");
     input.erase(input.begin(), input.begin() + 7);
@@ -103,6 +112,12 @@ public:
     }
   }
 
+  long factor() const { return modulus_; }
+
+  long count() const { return count_; }
+
+  void set_game(game& g) { game_ = &g; }
+
 private:
   int apply(int worry) const
   {
@@ -111,6 +126,8 @@ private:
 
   std::pair<int, int> step()
   {
+    count_++;
+
     auto worry = items_.front();
     items_.pop();
 
@@ -121,6 +138,8 @@ private:
 
     return {monkey, worry};
   }
+
+  long count_;
 
   std::queue<int>              items_;
   std::optional<int>           lhs_;
@@ -133,15 +152,50 @@ private:
   game* game_;
 };
 
+void game::add(monkey m)
+{
+  monkeys_.push_back(m);
+  monkeys_.back().set_game(*this);
+  field_ *= m.factor();
+}
+
 void game::throw_to(int monkey, int worry)
 {
   monkeys_.at(monkey).receive(worry);
 }
 
+void game::play_rounds(long n)
+{
+  for (auto i = 0; i < n; ++i) {
+    for (auto& m : monkeys_) {
+      m.execute();
+    }
+  }
+}
+
+long game::score() const
+{
+  auto scores = std::vector<long> {};
+
+  for (auto m : monkeys_) {
+    scores.push_back(m.count());
+  }
+
+  std::sort(scores.begin(), scores.end(), std::greater<long>());
+
+  return scores.at(0) * scores.at(1);
+}
+
 int main()
 {
   auto input = utils::get_lines();
+  auto g     = game();
+
   while (!input.empty()) {
     auto m = monkey(input);
+    g.add(m);
   }
+
+  g.play_rounds(20);
+  fmt::print("{}\n", g.score());
 }

96cc74c9310f9f1574df5ad438b9f6a65750935e
Bruce Collie <brucecollie82@gmail.com>
2024-12-11 10:06:28 +0000

    2024 day 11 2

--- 2024/src/day11.cpp
+++ 2024/src/day11.cpp
@@ -32,28 +32,56 @@ constexpr std::pair<std::uint64_t, std::uint64_t> split(std::uint64_t i)
 static_assert(split(17) == std::pair {1, 7});
 static_assert(split(510613) == std::pair {510, 613});
 
-void naive_step(std::list<std::uint64_t>& stones)
+template <typename Arr>
+void fill(Arr& table, std::uint64_t n, std::uint64_t steps)
 {
-  for (auto it = stones.begin(); it != stones.end(); ++it) {
-    if (*it == 0) {
-      *it = 1;
-    } else if (even_digits(*it)) {
-      auto [left, right] = split(*it);
-      stones.insert(it, left);
-      auto right_it = stones.insert(it, right);
-      stones.erase(it);
-      it = right_it;
+  for (auto i = 0u; i <= steps; ++i) {
+    if (table[i][n] != 0) {
+      continue;
+    }
+
+    if (i == 0) {
+      table[i][n] = 1;
     } else {
-      *it *= 2024;
+      if (n == 0) {
+        fill(table, 1, steps - 1);
+        table[i][n] = table[i - 1][1];
+      } else if (even_digits(n)) {
+        auto [left, right] = split(n);
+        fill(table, left, steps - 1);
+        fill(table, right, steps - 1);
+        table[i][n] = table[i - 1][left] + table[i - 1][right];
+      } else {
+        fill(table, n * 2024, steps - 1);
+        table[i][n] = table[i - 1][n * 2024];
+      }
     }
   }
+}
+
+template <std::size_t max_steps>
+std::uint64_t dp_step(std::list<std::uint64_t> const& stones)
+{
+  // table[s][n] = size of n after s steps
+  auto table = std::array<
+      std::unordered_map<std::uint64_t, std::uint64_t>, max_steps + 1> {};
+
+  for (auto s : stones) {
+    fill(table, s, max_steps);
+  }
+
+  auto sum = std::uint64_t {0};
+  for (auto s : stones) {
+    sum += table[max_steps][s];
+  }
+
+  return sum;
 }
 
 int main()
 {
   auto in = read_input();
-  for (auto i = 0u; i < 25; ++i) {
-    naive_step(in);
-  }
-  fmt::print("{}\n", in.size());
+
+  fmt::print("{}\n", dp_step<25>(in));
+  fmt::print("{}\n", dp_step<75>(in));
 }
